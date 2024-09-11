e5840ade01ba90b3ffaa47b58bc860dadbc8d69c
Al3jandr01405 <alejandrorolon1405@gmail.com>
2024-09-10 20:25:02 -0600

    Subiendo el archivo de python

---
+++ codigo.py
@@ -0,0 +1,7 @@
+nombre='Rodolfo'
+apellido='Rolón'
+saludo='Hola'
+despedida="Adios"
+
+print (f"{saludo} {nombre} {apellido}, ¿Como estas")
+print (f"{despedida} {nombre} {apellido}, que te vaya bien")
